c7b5ed04b110304411d0e4e0a3b9efae36f635aa
kielon <mosadowski23@gmail.com>
2023-03-06 18:50:08 +0100

    created typography component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import { Typography } from "./components/Typography/Typography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Typography variant="BodyL">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
+        sunt possimus obcaecati magni vero, itaque excepturi voluptates
+        voluptatem distinctio debitis!
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
