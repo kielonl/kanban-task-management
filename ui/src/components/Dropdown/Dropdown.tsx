823ea5cf6b9952c63f17d37f08cc2bb43dd1d0b9
kielon <mosadowski23@gmail.com>
2023-03-06 17:44:38 +0100

    added overflow scroll and max height to dropdown

--- ui/src/components/Dropdown/Dropdown.tsx
+++ ui/src/components/Dropdown/Dropdown.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { ArrowDown } from "../../assets/icons/arrowDown";
+import { ArrowDown } from "../../assets/icons/ArrowDown";
 import { ArrowUp } from "../../assets/icons/ArrowUp";
 import "./Dropdown.scss";
 
